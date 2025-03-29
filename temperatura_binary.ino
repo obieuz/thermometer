1cb3968c56bbb48cd69075f5e895603a3efd01d2
Robert Zając <98836348+obieuz@users.noreply.github.com>
2025-03-29 19:26:30 +0100

    Added code
    Added necesarry code to run thermometer

---
+++ temperatura_binary.ino
@@ -0,0 +1,76 @@
+#include <string.h>
+const int analogPin = A0;
+
+void setup_digital_pins(){
+  for(int i=2;i<7;i++){
+    pinMode(i,OUTPUT);
+  }
+}
+
+String reverse_string(String value){
+  String output = "";
+  for(int i=value.length()-1;i>=0;i--){
+      output += value[i];
+  }
+  return output;
+}
+
+String convert_dec_to_binary(int value){
+  String binary_val = "";
+
+  while(value != 1){
+    if(value % 2 == 0){
+      binary_val += "0";
+    }
+    else{
+      binary_val += "1";
+    }
+    value = value / 2;
+  }
+  binary_val += "1";
+  String output = reverse_string(binary_val);
+  return output;
+}
+
+void set_digital_to_low(){
+  for(int i=2;i<7;i++){
+    digitalWrite(i,LOW);
+  }
+}
+
+void show_temperature(String value_in_binary){
+  int digitalPin_index = 2;
+  for(int i=value_in_binary.length()-1;i>=0;i--){
+    Serial.println("");
+    Serial.print(value_in_binary[i]);
+    if(value_in_binary[i] == '1'){
+      digitalWrite(digitalPin_index,HIGH);
+    }
+    digitalPin_index+=1;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  setup_digital_pins();
+}
+
+void loop() {
+  set_digital_to_low();
+  int sensorVal=analogRead(analogPin);
+
+  float voltage = (sensorVal/1024.0)*5.0;
+
+  int temperature=(voltage-.5)*100;
+  Serial.println("");
+  Serial.print("Temperatura : ");
+  Serial.print(temperature);
+  Serial.print(", binary : ");
+
+  String binary = convert_dec_to_binary(temperature);
+  Serial.print(binary);
+
+  show_temperature(binary);
+
+  delay(10000);
+}
